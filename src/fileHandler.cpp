e57b1ab1614c3ce619374e5dd1626aa218d0491e
colletje <jeanne.collet@etu.utc.fr>
2025-06-10 10:21:05 +0200

    debbug, reste une erreur dans fileHandler, je crois qu'il faut passer un ptr mais à verif je ne suis pas sure

--- src/fileHandler.cpp
+++ src/fileHandler.cpp
@@ -87,7 +87,7 @@ static json toJsonPartie(const Partie& partie) {
                     delete action;
                 }
                 else {
-                    std::cerr << "[ERREUR] Tuile non trouvée à la position "
+                    std::cerr << "[ERREUR] Tuile non trouvee a la position "
                         << posDerniereTuile.x << "," << posDerniereTuile.y
                         << " pour placement du jeton en attente.\n";
                 }
@@ -161,7 +161,7 @@ static void fromJsonPartie(const nlohmann::json& j, Partie& partie) {
             }
         }
         catch (const std::exception& e) {
-            std::cerr << "[ERREUR] Chargement d'une action échoué : " << e.what() << "\n";
+            std::cerr << "[ERREUR] Chargement d'une action echoue : " << e.what() << "\n";
         }
     }
 }
